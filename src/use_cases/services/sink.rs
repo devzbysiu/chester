9b3cca70573b6cf6f6f46e72ccf35935751a7899
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2023-02-05 11:45:44 +0100

    test: Add case of pending tests

--- src/use_cases/services/sink.rs
+++ src/use_cases/services/sink.rs
@@ -45,7 +45,7 @@ mod test {
     use anyhow::Result;
 
     #[test]
-    fn successfull_result_is_written_to_repo() -> Result<()> {
+    fn when_tests_succeed_success_is_writen_to_repo() -> Result<()> {
         // given
         let shim = create_test_shim()?;
         let (repo_spy, repo) = tracked(&working());
@@ -62,7 +62,7 @@ mod test {
     }
 
     #[test]
-    fn failed_result_is_written_to_repo() -> Result<()> {
+    fn when_tests_fail_failure_is_written_to_repo() -> Result<()> {
         // given
         let shim = create_test_shim()?;
         let (repo_spy, repo) = tracked(&working());
@@ -74,6 +74,23 @@ mod test {
 
         // then
         assert!(repo_spy.write_called_with_val(&Status::Failure));
+
+        Ok(())
+    }
+
+    #[test]
+    fn when_tests_start_pending_is_written_to_repo() -> Result<()> {
+        // given
+        let shim = create_test_shim()?;
+        let (repo_spy, repo) = tracked(&working());
+        ResultsSinkShell::new(shim.bus()).run(repo.write());
+
+        // when
+        shim.simulate_change()?;
+        shim.ignore_event()?;
+
+        // then
+        assert!(repo_spy.write_called_with_val(&Status::Pending));
 
         Ok(())
     }
