8b127dbd52eb20cb55d4d62c677301cad1121579
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2023-03-16 15:16:02 +0100

    test: Add tests of DefaultCheckRunner

--- src/data_providers/check_runner.rs
+++ src/data_providers/check_runner.rs
@@ -21,7 +21,7 @@ impl CRunner for DefaultCheckRunner {
     fn run(&self, repo_root: RepoRoot) -> Result<CheckRunStatus, CheckErr> {
         let repo_root = repo_root.to_string();
         debug!("running check in {repo_root}");
-        let Ok(status) = self.cfg.check_cmd.status(repo_root) else {
+        let Ok(status) = self.cfg.check_cmd.status(&repo_root) else {
             debug!("command failed");
             return Ok(CheckRunStatus::Failure);
         };
@@ -34,58 +34,56 @@ impl CRunner for DefaultCheckRunner {
     }
 }
 
-// TODO: Add tests
+#[cfg(test)]
+mod test {
+    use super::*;
 
-// #[cfg(test)]
-// mod test {
-//     use super::*;
+    use crate::configuration::config::{Cmd, ConfigBuilder};
+    use crate::configuration::tracing::init_tracing;
 
-//     use crate::configuration::config::{Cmd, ConfigBuilder};
-//     use crate::configuration::tracing::init_tracing;
+    use anyhow::Result;
+    use cmd_lib::run_cmd;
+    use tempfile::tempdir;
 
-//     use anyhow::Result;
-//     use cmd_lib::run_cmd;
-//     use tempfile::tempdir;
+    #[test]
+    fn when_check_fail_then_failure_status_is_returned() -> Result<()> {
+        // given
+        init_tracing();
+        let cfg = ConfigBuilder::default()
+            .check_cmd(Cmd::new("cargo", &["check"]))
+            .build()?;
+        let runner = DefaultCheckRunner::make(cfg);
+        let invalid_repo_root = RepoRoot::new("/not/existing/path");
 
-//     #[test]
-//     fn when_tests_fail_then_failure_status_is_returned() -> Result<()> {
-//         // given
-//         init_tracing();
-//         let cfg = ConfigBuilder::default()
-//             .tests_cmd(Cmd::new("cargo", &["test"]))
-//             .build()?;
-//         let runner = DefaultTestRunner::make(cfg);
-//         let invalid_repo_root = RepoRoot::new("/not/existing/path");
+        // when
+        let res = runner.run(invalid_repo_root)?;
 
-//         // when
-//         let res = runner.run_all(invalid_repo_root)?;
+        // then
+        assert_eq!(res, CheckRunStatus::Failure);
 
-//         // then
-//         assert_eq!(res, TestsRunStatus::Failure);
+        Ok(())
+    }
 
-//         Ok(())
-//     }
+    #[test]
+    fn when_check_succeed_then_success_status_is_returned() -> Result<()> {
+        // given
+        init_tracing();
+        let tmpdir = tempdir()?;
+        let tmpdir_path = tmpdir.path();
+        run_cmd!(cd $tmpdir_path ; cargo new test_project)?;
+        let cfg = ConfigBuilder::default()
+            .check_cmd(Cmd::new("cargo", &["check"]))
+            .build()?;
+        let runner = DefaultCheckRunner::make(cfg);
+        let project_path = tmpdir_path.join("test_project");
+        let root = RepoRoot::new(project_path);
 
-//     #[test]
-//     fn when_tests_succeed_then_success_status_is_returned() -> Result<()> {
-//         // given
-//         init_tracing();
-//         let tmpdir = tempdir()?;
-//         let tmpdir_path = tmpdir.path();
-//         run_cmd!(cd $tmpdir_path ; cargo new test_project)?;
-//         let cfg = ConfigBuilder::default()
-//             .tests_cmd(Cmd::new("cargo", &["test"]))
-//             .build()?;
-//         let runner = DefaultTestRunner::make(cfg);
-//         let project_path = tmpdir_path.join("test_project");
-//         let root = RepoRoot::new(project_path);
+        // when
+        let res = runner.run(root)?;
 
-//         // when
-//         let res = runner.run_all(root)?;
+        // then
+        assert_eq!(res, CheckRunStatus::Success);
 
-//         // then
-//         assert_eq!(res, TestsRunStatus::Success);
-
-//         Ok(())
-//     }
-// }
+        Ok(())
+    }
+}
