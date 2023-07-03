08150417733696b03c3420c8c32fa860babb94da
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2023-07-03 19:19:18 +0200

    docs: Add docs for check_runner

--- src/data_providers/check_runner.rs
+++ src/data_providers/check_runner.rs
@@ -5,6 +5,9 @@ use crate::use_cases::check_runner::{CRunner, CheckRunStatus, CheckRunner};
 
 use tracing::{debug, instrument};
 
+/// It runs the command for a check stage. Command is passed in via `Config::check_cmd`.
+///
+/// The execution can fail in two ways. See the [`DefaultCheckRunner::run`] for details.
 #[derive(Debug)]
 pub struct DefaultCheckRunner {
     cfg: Config,
@@ -18,6 +21,12 @@ impl DefaultCheckRunner {
 
 impl CRunner for DefaultCheckRunner {
     #[instrument(skip(self))]
+    /// Executes the `check_cmd` on the path specified by `repo_root`.
+    ///
+    /// The execution can fail in two ways:
+    /// - there is an error while executing `check_cmd` command
+    /// - the command succeeds, but there are issues with the code
+    /// (`check_cmd` exits with non-zero status code).
     fn run(&self, repo_root: RepoRoot) -> Result<CheckRunStatus, CheckErr> {
         debug!("running check in {repo_root}");
         let Ok(status) = self.cfg.check_cmd.status(&repo_root) else {
